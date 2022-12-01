6f19064e03987774ffabb9b97c5ce2d483d91dad
whalen-nftmoa <whalen@nftmoa.io>
2022-12-01 10:57:14 +0900

    [REALESTATE-14] fognet 환율 api 변경

--- src/main/java/com/labshigh/realestate/internal/api/exchange/service/ExchangeService.java
+++ src/main/java/com/labshigh/realestate/internal/api/exchange/service/ExchangeService.java
@@ -32,7 +32,7 @@ public class ExchangeService {
 
   public ExchangeVirtualResponseModel getVirtual(ExchangeGetVirtualRequestModel requestModel) {
 
-    if (requestModel.getExchangeName().equals("FOG_WON")) {
+    if (requestModel.getExchangeName().equals("FoGNET_USD")) {
 
       ExchangeVirtual2Dao exchangeVirtual2Dao = exchangeVirtual2Mapper.get(
           ExchangeVirtual2Dao.builder().name(requestModel.getExchangeName()).build());
